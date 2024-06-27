fdb1cac0dc7bf7cd1fa1266e561617cc19ac4548
Mftr96 <matteo.federici96@gmail.com>
2024-06-27 17:10:55 +0200

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="../../public/style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ciao Laravel </title>
+</head>
+<body>  
+    <h1>eccoci qui con laravel </h1>
+    <p>ciao</p>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquid eius illo libero quidem commodi error deleniti, ipsa exercitationem assumenda tempora earum nihil dolorem voluptatum maiores dolore eum. Illum, amet ipsam.
+    Consectetur dicta quis suscipit ipsa dolor, sequi impedit quibusdam. Laborum, sed quia ex maiores iste et beatae officia enim sint cupiditate possimus provident itaque exercitationem, molestias recusandae fuga dolor? Tempore.
+    Nostrum fugit, sunt autem necessitatibus quia dicta ab, reprehenderit vel eos laboriosam illo impedit harum ullam! Autem nam dolore, nemo alias eius doloribus rerum expedita vel unde, atque ratione porro.</p>
+    
+</body>
+</html>
